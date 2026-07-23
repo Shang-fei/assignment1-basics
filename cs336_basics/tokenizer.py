0f22c871bad298250a9a5ef8f83ac68d60a5aaec
shangfei <3335288021@qq.com>
2026-07-23 12:57:21 +0800

    完成tokenizer

---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,90 @@
+from collections.abc import Iterable
+from typing import Iterator
+
+import regex as re
+class Tokenizer:
+
+    def __init__(self, vocab:dict[int, bytes], merges:list[tuple[bytes, bytes]], special_tokens:list[str]):
+        self.vocab = vocab
+        self.bytes2int = {value: key for key ,value in self.vocab.items()}
+
+        self.merges = merges
+        self.merge_rank = {pair: i for i, pair in enumerate(merges)}
+
+        self.special_tokens = special_tokens
+
+        if special_tokens is not None:
+            self.special_tokens = sorted(special_tokens, key=len, reverse=True)
+
+
+    def from_files(cls, vocab_filepath:str, merges_filepath:str, special_tokens=None):
+        with open(vocab_filepath, "rb") as f:
+            vocab = f.read()
+        
+        with open(merges_filepath, 'rb') as f:
+            merges = f.read()
+
+        special_token = sorted(special_token, key=len, reverse=True)
+        return cls(vocab, merges, special_tokens)
+
+
+    def encode(self, text: str) -> list[int]:
+        ids:list[int] = []
+        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+        if self.special_tokens is not None:
+            pattern = "(" +  '|'.join([re.escape(token) for token in self.special_tokens])  + ")"
+            text_list = re.split(pattern, text)
+        else:
+            text_list = [text]
+
+        for text in text_list:
+            if self.special_tokens is not None:
+                token_bytes = text.encode('utf-8')
+                if (token_idx:=self.bytes2int.get(token_bytes, None)) is not None:
+                    ids.append(token_idx)
+                    continue
+            
+            for pretoken in re.findall(PAT, text):
+                token_bytes = [bytes([c]) for c in pretoken.encode('utf-8')]
+
+                while len(token_bytes) >= 2:
+                    merge_pair = None
+                    merge_rank = float('inf')
+                    for start, end in zip(token_bytes[:-1], token_bytes[1:]):
+                        pair = (start, end)
+                        rank = self.merge_rank.get(pair, None)
+                        if rank is not None and rank < merge_rank:
+                            merge_rank = rank
+                            merge_pair = pair
+
+                    if merge_pair is None:
+                        break
+
+                    new_token_bytes = []
+
+                    i = 0
+                    while i < len(token_bytes):
+                        if (i+1) < len(token_bytes) and (token_bytes[i], token_bytes[i+1]) == merge_pair:
+                            new_token_bytes.append(token_bytes[i]+token_bytes[i+1])
+                            i += 2
+
+                        else:
+                            new_token_bytes.append(token_bytes[i])
+                            i += 1
+                    token_bytes = new_token_bytes
+
+                for b in token_bytes:
+                    ids.append(self.bytes2int[b])
+
+        return ids
+
+        
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            yield from self.encode(text)
+    
+
+    def decode(self, ids: list[int]) -> str:
+        token_bytes = b"".join(self.vocab[token_id] for token_id in ids)
+        return token_bytes.decode("utf-8", errors="replace")
--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,119 +1,33 @@
-import os
-import regex as re
+import time
+import resource
+import pickle
 
-from collections import Counter, defaultdict
-from functools import partial
-from multiprocessing import Pool
-from cs336_basics.pretokenization_example import find_chunk_boundaries
+from bpe import train_bpe
 
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-
-def init_vocab(special_tokens: list[str]):
-    init_vacab = {}
-    for i in range(256):
-        init_vacab[i] = bytes([i])
-
-    for i, token_str in enumerate(special_tokens, start=256):
-        token_bytes = token_str.encode("utf-8")
-        init_vacab[i] = token_bytes
-    return init_vacab
-
-def create_chunks(input_path: str):
-    with open(input_path, "rb") as f:
-        num_processes = 4
-        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-        for start, end in zip(boundaries[:-1], boundaries[1:]):
-            f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            yield chunk
-
-
-def prepare_data(chunks:list[str], special_tokens:list[str]):
-    worker = partial(get_pretoken_counter, special_tokens=special_tokens)
-    with Pool(processes=4) as pool:
-        pretoken_counter_list = pool.map(worker, chunks)
-
-    pretoken_counter = Counter()
-    for counter in pretoken_counter_list:
-        pretoken_counter.update(counter)
-
-    pairs_counter = Counter()
-    for token, count in pretoken_counter.items():
-        for start, end in zip(token[:-1], token[1:]):
-            pairs_counter[(start, end)] += count
-
-    return pairs_counter, pretoken_counter
-
-
-def get_pretoken_counter(chunk: str, special_tokens: list[str]):
-    local_pretoken_counter = Counter()
-    clean_chunk_list = re.split('|'.join([re.escape(token) for token in special_tokens]), chunk)
-
-    for chunk in clean_chunk_list:
-        pretoken_list = re.findall(PAT, chunk)
-        for pretoken in pretoken_list:
-            pretoken_bytes = pretoken.encode('utf-8')
-            token = tuple(bytes([byte_value]) for byte_value in pretoken_bytes)
-            local_pretoken_counter[token] += 1
-
-    return local_pretoken_counter
-
-
-def merge(vacab:dict[int, bytes], merges:list[(bytes, bytes)], pairs_counter:Counter[tuple], pretoken_counter:Counter[tuple]):
-    selected_pair = max(pairs_counter, key=lambda pair:(pairs_counter.get(pair), pair))
-
-    selected_token = []
-    for token in pretoken_counter:
-        for start, end in zip(token[:-1], token[1:]):
-            if (start, end) == selected_pair:
-                selected_token.append(token)
-                break
-    
-    for token in selected_token:
-        count = pretoken_counter.get(token)
-        for i in range(len(token)-1):
-            pairs_counter[(token[i], token[i+1])] -= count
-    
-    for token in selected_token:
-        count = pretoken_counter.get(token)
-        new_token = []
-        i=0
-        while i < len(token):
-            if (i + 1 < len(token) and (token[i], token[i + 1]) == selected_pair):
-                new_token.append(token[i] + token[i + 1])
-                i += 2
-            else:
-                new_token.append(token[i])
-                i += 1
-
-        for i in range(len(new_token)-1):
-            pairs_counter[(new_token[i], new_token[i+1])] += count
-        
-        pretoken_counter.pop(token, None)
-        pretoken_counter[tuple(new_token)] += count
-
-    vacab[len(vacab)] = (selected_pair[0] + selected_pair[1])
-    merges.append(selected_pair)
-
-
-def train_bpe(
-    input_path: str | os.PathLike,
-    vocab_size: int,
-    special_tokens: list[str],
-    **kwargs,
-) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-
-
-    merges = []
-    vocab = init_vocab(special_tokens)
-    chunks = create_chunks(input_path=input_path)
-    pairs_counter, pretoken_counter = prepare_data(chunks = chunks, special_tokens=special_tokens)
-
-    while len(vocab) < vocab_size:
-        merge(vocab, merges, pairs_counter, pretoken_counter)
-
+def train(input_path:str, vocab_size:int, special_tokens:list[str]):
+    vocab, merges = train_bpe(input_path, vocab_size, special_tokens)
     return vocab, merges
+
+if __name__ == "__main__":
+    input_path = "/home/shangfei/shang/cs336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt"
+    vocab_size = 10000
+    special_tokens = ["<|endoftext|>"]
+
+    start = time.perf_counter()
+    vocab, merges = train(input_path, vocab_size, special_tokens)
+    end = time.perf_counter()
+
+    cpu_peak_gb = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/ 1024**2)
+
+    print(f"训练用时:{end - start}")
+    print(f"CPU 峰值内存: {cpu_peak_gb:.3f} GB")
+
+    with open("../output/tiny_stores_vocab.pkl", "wb") as f:
+        pickle.dump(vocab, f)
+    with open("../output/tiny_stores_merges.pkl", "wb") as f:
+        pickle.dump(merges, f)
     
+    longest_token_id = max(vocab, key = lambda idx : len(vocab.get(idx)))
+    longest_token_bytes = vocab.get(longest_token_id)
+    longest_token_str = longest_token_bytes.decode('utf-8')
+    print(longest_token_str)
